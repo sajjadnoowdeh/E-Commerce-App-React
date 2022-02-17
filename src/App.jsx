b7b88b1c0931baa9ff5f8051c26bed326d06152f
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-02-17 13:33:14 +0330

    fix: add routers page

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Route, Routes ,Navigate} from 'react-router-dom'
 const App = () => {
+  const user = true;
   return (
-    <div>
-      <Cart />
-    </div>
+    <Router>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route  path="/products/:category" element={<ProductList/>}/>
+        <Route  path="/product/:id" element={<Product/>}/>
+        <Route  path="/cart" element={<Cart/>}/>
+        <Route  path="/register" element={user ? <Navigate to={'/'} /> : <Register  />}/>
+        <Route  path="/login" element={user ? <Navigate to={"/"} />: <Login/>}/>
+      </Routes>
+  </Router>
   );
 };
 
