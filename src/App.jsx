879aae419dbaea3ac67ed81c6af5d9e4de0f61d8
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-02-18 15:44:15 +0330

    fix: get user from redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import Home from "./pages/Home";
-import ProductList from "../src/pages/ProductList"
+import ProductList from "../src/pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import { BrowserRouter as Router, Route, Routes ,Navigate} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Success from "./pages/Success";
+
+import { useSelector } from "react-redux";
 const App = () => {
-  const user = true;
+  const user = useSelector((state) => state.reducer.user.currentUser);
+  console.log(user);
   return (
     <Router>
       <Routes>
-        <Route  path="/" element={<Home/>}/>
-        <Route  path="/products/:category" element={<ProductList/>}/>
-        <Route  path="/product/:id" element={<Product/>}/>
-        <Route  path="/cart" element={<Cart/>}/>
-        <Route  path="/register" element={user ? <Navigate to={'/'} /> : <Register  />}/>
-        <Route  path="/login" element={user ? <Navigate to={"/"} />: <Login/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/success" element={<Success />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to={"/"} /> : <Register />}
+        />
+        <Route
+          path="/login"
+          element={user ? <Navigate to={"/"} /> : <Login />}
+        />
       </Routes>
-  </Router>
+    </Router>
   );
 };
 
